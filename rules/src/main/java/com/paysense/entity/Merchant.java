8eaa7e8881811cc0d8ad78011b0455e17d26aa1b
user2 <>
2015-07-30 15:41:55 +0000

    {/rules/src/main/java/com/paysense/entity/Merchant.java}

--- rules/src/main/java/com/paysense/entity/Merchant.java
+++ rules/src/main/java/com/paysense/entity/Merchant.java
@@ -11,4 +11,38 @@ public class Merchant implements java.io.Serializable {
     public Merchant() {
     }
 
+private String id;
+	
+	private String name;
+
+	private double totalExposureAmount;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getTotalExposureAmount() {
+		return totalExposureAmount;
+	}
+
+	public void setTotalExposureAmount(double totalExposureAmount) {
+		this.totalExposureAmount = totalExposureAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "Merchant [id=" + id + ", name=" + name + ", totalExposureAmount=" + totalExposureAmount + "]";
+	}
 }
